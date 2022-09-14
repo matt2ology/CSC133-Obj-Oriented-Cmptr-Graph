62d7c37118e71cc91bdf2db19394c98ea5de0205
Matthew Tran <matthewtran@Matthews-MacBook-Air.local>
2022-09-14 16:16:23 -0700

    Refactored Class name to match file name

--- week-03-04/homework02/SimplePaintObjects.java
+++ week-03-04/homework02/SimplePaintObjects.java
@@ -17,7 +17,7 @@ import javafx.scene.paint.Color;
  * clicking and dragging in a large white area that occupies most of the
  * canvas.
  */
-public class SimpleToolPaint extends Application {
+public class SimplePaintObjects extends Application {
     public static void main(String[] args) {
         launch(); // This main routine allows this class to be run as a program
     }
