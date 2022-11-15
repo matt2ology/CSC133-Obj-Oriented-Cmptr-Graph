3ce34129b3f835d3c451fc583a3b3620ede3a775
matt2ology <34324060+matt2ology@users.noreply.github.com>
2022-11-15 11:35:03 -0800

    All needed imports

--- week-09-10-a2-rainmaker-course-project/a2-rainmaker/GameApp.java
+++ week-09-10-a2-rainmaker-course-project/a2-rainmaker/GameApp.java
@@ -1,12 +1,23 @@
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.geometry.Dimension2D;
 import javafx.geometry.Point2D;
 import javafx.scene.Group;
-import javafx.scene.Node;
-import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.Node;
 import javafx.scene.paint.Color;
+import javafx.scene.Scene;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Scale;
 import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 
