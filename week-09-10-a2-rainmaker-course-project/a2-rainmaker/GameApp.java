c6c952497c93676c2f8569dcd367c72866fc6083
matt2ology <34324060+matt2ology@users.noreply.github.com>
2022-11-25 01:33:12 -0800

    Added GameText class

--- week-09-10-a2-rainmaker-course-project/a2-rainmaker/GameApp.java
+++ week-09-10-a2-rainmaker-course-project/a2-rainmaker/GameApp.java
@@ -73,6 +73,22 @@ abstract class FixedObject extends GameObject {
     }
 }
 
+class GameText extends Text {
+    private String FONT_OF_CHOICE = "Helvetica";
+    private Scale scale = new Scale();
+    public GameText(String text, int textFontSize) {
+        super(text);
+        this.setFont(Font.font(FONT_OF_CHOICE, textFontSize));
+        this.scale.setY(-1);
+        this.getTransforms().add(scale);
+        // set the origin of the text to the center
+        this.setX(-this.getLayoutBounds().getWidth() / 2);
+        // 4 is a magic number that works for Helvetica because
+        // the letters are taller than it is wide and we want to center it
+        this.setY(this.getLayoutBounds().getHeight() / 4);
+    }
+}
+
 /**
  * Game
  */
